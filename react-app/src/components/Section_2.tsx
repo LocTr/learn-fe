66c1dd0f525360f6586fcbdb03a93f4aa53c5e4a
Loc <ttloc1999@gmail.com>
2025-08-21 17:32:11 +0700

    continue...

---
+++ react-app/src/components/Section_2.tsx
@@ -0,0 +1,11 @@
+export default function Section2() {
+    return (
+        <div className="h-[606px] bg-brand-beige ">
+            <div className="text-[14px] font-bold font-default">BOAS-VINDAS A DOCTORCARE 👋</div>
+            <div>Assistência médica simplificada para todos</div>
+            <div>Os médicos da DoctorCare vão além dos sintomas para tratar a causa raiz de sua doença e proporcionar uma cura a longo prazo.</div>
+            <div>button</div>
+            <img src="assets/images/woman-img.png" className="w-[300px] h-[400px] object-cover" />
+        </div>
+    )
+}
--- react-app/src/pages/HomePage.tsx
+++ react-app/src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
 import Appbar from "../components/appbar";
 import Section1 from "../components/Section_1";
+import Section2 from "../components/Section_2";
 
 export default function HomePage() {
     return <>
         <Appbar />
         <Section1 />
+        <Section2 />
+
     </>;
 }
